7c3fe782b314564999fd1ac19f53b61ccf168bb5
Franklin Bastos <franklin.wesley.bastos@gmail.com>
2017-03-26 19:31:38 -0300

    upload de arquivos

--- client/app.js
+++ client/app.js
@@ -1,69 +1,73 @@
 (function() {
 	angular
-		.module('simpledropbox', ['angularFileUpload'])
-		.controller('AppController', controller);
+	.module('simpledropbox', ['angularFileUpload'])
+	.controller('AppController', controller)
+	.factory('AppService', service);
 
-	controller.$inject = ['$scope', 'FileUploader'];
+	controller.$inject = ['$scope', 'FileUploader', 'AppService'];
 
-	function controller ($scope, FileUploader) {
-		var uploader = $scope.uploader = new FileUploader({
-		    url: 'upload.php'
-		});
+	function controller ($scope, FileUploader, AppService) {
 
-		// FILTERS
-	      
-		// a sync filter
-		uploader.filters.push({
-		    name: 'syncFilter',
-		    fn: function(item /*{File|FileLikeObject}*/, options) {
-		        console.log('syncFilter');
-		        return this.queue.length < 10;
-		    }
-		});
-	      
-		// an async filter
-		uploader.filters.push({
-		    name: 'asyncFilter',
-		    fn: function(item /*{File|FileLikeObject}*/, options, deferred) {
-		        console.log('asyncFilter');
-		        setTimeout(deferred.resolve, 1e3);
-		    }
-		});
+		var plant_picture;
+
+		$scope.uploader = new FileUploader();
 
 		// CALLBACKS
+		$scope.uploader.onAfterAddingFile = function(fileItem) {
+			$scope.fileError = false;
+			plant_picture = fileItem._file;
+		};
 
-		uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
-		    console.info('onWhenAddingFileFailed', item, filter, options);
+		$scope.uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+			$scope.fileError = true;
+			$timeout(function () { $scope.fileError = false; }, 1500);
 		};
-		uploader.onAfterAddingFile = function(fileItem) {
-		    console.info('onAfterAddingFile', fileItem);
+
+		$scope.uploader.onAfterAddingAll = function(addedFileItems) {
+			if ($scope.uploader.getNotUploadedItems().length > 1) {
+				$scope.uploader.removeFromQueue(0);
+			}
 		};
-		uploader.onAfterAddingAll = function(addedFileItems) {
-		    console.info('onAfterAddingAll', addedFileItems);
-		};
-		uploader.onBeforeUploadItem = function(item) {
-		    console.info('onBeforeUploadItem', item);
-		};
-		uploader.onProgressItem = function(fileItem, progress) {
-		    console.info('onProgressItem', fileItem, progress);
-		};
-		uploader.onProgressAll = function(progress) {
-		    console.info('onProgressAll', progress);
-		};
-		uploader.onSuccessItem = function(fileItem, response, status, headers) {
-		    console.info('onSuccessItem', fileItem, response, status, headers);
-		};
-		uploader.onErrorItem = function(fileItem, response, status, headers) {
-		    console.info('onErrorItem', fileItem, response, status, headers);
-		};
-		uploader.onCancelItem = function(fileItem, response, status, headers) {
-		    console.info('onCancelItem', fileItem, response, status, headers);
-		};
-		uploader.onCompleteItem = function(fileItem, response, status, headers) {
-		    console.info('onCompleteItem', fileItem, response, status, headers);
-		};
-		uploader.onCompleteAll = function() {
-		    console.info('onCompleteAll');
-		};
+
+		$scope.upload = function () {
+			var data = new FormData();
+			data.append('file', plant_picture);
+			AppService.upload(data, function (error, data) {
+				if(error) {
+					$scope.fileError = error.code;
+					return;
+				}
+
+				$scope.success = true;
+			});
+		}
 	}
+
+	service.$inject = ['$http'];
+
+	function service($http) {
+		
+		const URI = '/upload';
+
+		/**
+        * Functions declarations on the top for better reading and understanding
+        * */
+        return {
+        	upload: upload
+        };
+
+        function upload(data, callback) {
+        	return $http({
+        		method: 'POST',
+        		url: URI,
+        		data: data,
+        		transformRequest: angular.identity,
+        		headers: {'Content-Type': undefined}
+        	}).then(function(response){
+        		callback(null, response.data);
+        	}, function(error){
+        		callback(error.data);
+        	});
+        }
+    }
 })();
